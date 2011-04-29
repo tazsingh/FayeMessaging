3e57817f5e61e69925b701d5ef3bd3d27326011f
dann toliver <dann@bentobox.net>
2011-04-28 21:09:47 -0400

    more js testing... maybe it works now?

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,18 +1,43 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
+
 $(function() {
+  history = [];
+  current_value = '';
+  current_position = 0;
+  
   var faye = new Faye.Client('http://192.168.1.149:9292/faye');
   faye.subscribe('/messages/new', function (data) {
-    $('#chat_box').append($("<p>" + data.text + "</p>"));
+    $('#chat_box').append($('<div class="message"><div class="timestamp">' + data.timestamp + '</div><div class="username">' + data.username + '</div><div class="text">' + data.text + '</div></div>'));
     $('#chat_box').scrollTop(1000000);
   });
   
   $('#message_submit').click(function(e) {
     e.preventDefault();
-    faye.publish('/messages/new', {text: $('#message_text').val()});
+    
+    history.unshift($('#message_text').val());
+    current_value = '';
+    current_position = 0;
+    
+    faye.publish('/messages/new', {
+      username: $('#message_username').val(),
+      timestamp: (new Date()).getTime(),
+      text: $('#message_text').val()
+    });
     $('#message_text').val("");
     return false;
   });
+  
+  // catch up arrow
+  // if(e.keyCode == 38) { // up arrow goes up in history
+  //   if(current_position == 0) {
+  //     current_value = $('#message_text').val();
+  //   }
+  //   
+  // }
+  // 
+  // // TODO: rewrite this:
+  // $('#command').livequery('keydown', function(e) {arrowStuff(e)});
   
 }); 
