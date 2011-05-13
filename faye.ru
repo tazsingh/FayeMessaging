fb5fa50602add451eb62238f16a37555e6729bf0
Ian Ha <ianha0@gmail.com>
2011-05-12 20:48:39 -0400

    Undid reference to interceptor in faye.ru. To be placed in it's own rackup file

--- faye.ru
+++ faye.ru
@@ -1,7 +1,3 @@
 require 'faye'
-require './lib/interceptor'
 
-faye_server = Faye::RackAdapter.new(:mount => '/faye', :timeout => 45)
-use Rack::CommonLogger
-use Rack::Interceptor
-run faye_server
+run Faye::RackAdapter.new(:mount => '/faye', :timeout => 45)
--- lib/interceptor.rb
+++ lib/interceptor.rb
@@ -6,16 +6,9 @@ module Rack
     def call(env)
       @req = Rack::Request.new(env)
       
-      if @req.post?
-        user_message = @req.params['text']
-        return amazon_book_title  if user_message.upcase.start_with?("/ISBN")
-      end
+      # EventMachine used here for subscription to faye
       
       @app.call(env)
-    end
-    
-    def amazon_book_title
-      [200, {'Content-type' => 'text'}, @req.params]
     end
   end
 end
