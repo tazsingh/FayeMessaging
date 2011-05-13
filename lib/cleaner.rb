f158976d0ed7fba254c63cd79ed7d5ee98af1bfc
Tasveer Singh <tsingh@zenapsis.com>
2011-05-12 21:36:24 -0400

    Don't use Rack for Faye channeling

--- lib/cleaner.rb
+++ lib/cleaner.rb
@@ -1,23 +1,25 @@
+require 'eventmachine'
 require 'faye'
 
-module Rack
-  class Cleaner
-    def initialize app
-      @faye_client = Faye::Client.new('http://192.168.1.149:9292')
-    end
+module CleanerConnection
+  def receive_data data
+    @faye_client = Faye::Client.new('http://192.168.1.149:9292')
 
-    def call env
-      EM::next_tick do
-        @faye_client.subscribe('/messages/dirty') do |message|
-          # TODO: CLEAN!!!!
+    @faye_client.subscribe('/messages/dirty') do |message|
+      # TODO: CLEAN!!!!
 
-          %w[text username timestamp].each do |key|
+      %w[text username timestamp].each do |key|
 
-          end
-
-          @faye_client.publish('/messages/clean', :text => message.inspect, :username => 'TESTING!!!', :timestamp => 0)
-        end
       end
+
+      puts 'gets here'
+
+      @faye_client.publish('/messages/clean', :text => message.text, :username => message.username,
+                           :timestamp => message.timestamp)
     end
   end
+end
+
+EM::run do
+  EM::start_server '0.0.0.0', 9294, CleanerConnection
 end
