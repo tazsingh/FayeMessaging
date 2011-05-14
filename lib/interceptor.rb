f4e522574f2cb3326a38dd2483cb3b3d425181b0
Ian Ha <ianha0@gmail.com>
2011-05-13 22:46:00 -0400

    Can now define more than one ISBN at once; uses em-synchrony for async HTTP fetches

--- lib/interceptor.rb
+++ lib/interceptor.rb
@@ -1,9 +1,9 @@
 require 'faye'
 require 'eventmachine'
-require 'em-http'
 require './lib/faye_messaging'
-require 'pp'
 require 'nokogiri'
+require 'em-synchrony'
+require 'em-synchrony/em-http'
 
 # Handles connection when client wants to submit book ISBN and have book title
 # published to their Faye feed
@@ -27,33 +27,29 @@ class  InterceptorConnection < EM::Connection
 private
 
   def get_isbn(data)
-    data.split(' ')[1]
+    isbns = data.split(' ')
+    isbns.shift
+    isbns
   end
   
-  # Asynchronously fetch book data given the ISBN
-  def send_amazon_title(isbn)
-    url = InterceptorConnection::ISBNDB_URL % isbn
-    async_fetch_and_publish(url) { |data| librarything_response_parser(data) }
-  end
-  
-  def async_fetch_and_publish(url, &block)
-    puts "requesting #{url}"
-    http = EventMachine::HttpRequest.new(url).get(:timeout => FayeMessaging::HTTP_TIMEOUT_IN_SEC)
-    http.errback do
-      puts "HTTP Error: Status #{http.response_header.status}"
-      send_data("") # empty string signals error
-    end
-    http.callback do
-      book_title = block.call(http.response)
-      @client.publish(FayeMessaging::PUBLISH_CLEAN_URI, :text => book_title)
+  # Asynchronously fetch book data given the ISBNs
+  def send_amazon_title(isbns)
+    EM.synchrony do
+      multi = EventMachine::Synchrony::Multi.new
+      
+      isbns.each_with_index do |isbn, index|
+        multi.add "page#{index}".to_sym, EventMachine::HttpRequest.new(InterceptorConnection::ISBNDB_URL % isbn).aget
+      end
+    
+      titles = multi.perform.responses[:callback].values.map { |h| response_parser(h.response) }
+      @client.publish(FayeMessaging::PUBLISH_CLEAN_URI, :text => titles.inspect)
       send_data("Book title published successfully!")
     end
   end
   
   # Returns book title given XML response back from isbndb.com
   # Returns empty string if title not found
-  def librarything_response_parser(data)
-    pp data
+  def response_parser(data)
     begin
       doc = Nokogiri::XML.parse(data)
       doc.xpath('//Title').first.content
@@ -64,10 +60,10 @@ private
 end
 
 if $0 == __FILE__
-  EM::run {
+  EM.synchrony do
     host = '0.0.0.0'
     port = 9292
     EM::start_server host, port, InterceptorConnection
     puts "Interceptor server STARTED on host #{host} and port #{port}"
-  }
+  end
 end
